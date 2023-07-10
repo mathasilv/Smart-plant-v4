8818ff39f854cbde870f51d5a5c81c552693b90f
NEXUS-06 <matheus_aparecido@discente.ufg.br>
2023-07-09 23:35:43 -0300

    new file:   .vscode/settings.json modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
-#include <header.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1106.h>
+#include <Fonts/FreeSerif12pt7b.h>
+#define OLED_RESET 4
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET 4
+Adafruit_SH1106 display(OLED_RESET);
+const int sensorUmid = A0;
+const int sensorLuz = A1;
 
 // Rosto Alegre - frame 0
 const unsigned char Alegre0[] PROGMEM = {
